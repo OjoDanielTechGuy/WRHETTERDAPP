adf5173c2b86bae0b0ffb09049f2676463e2a493
OJO DANIEL OLUWAGEBMIGA <danielbankole33@yahoo.com>
2022-12-02 15:25:02 +0000

    changed detials in whom to follow

--- wrheeterdapp-blockchain/lib/newsStatic.js
+++ wrheeterdapp-blockchain/lib/newsStatic.js
@@ -1,46 +1,45 @@
 export const news = [
   {
-    category: 'HiMark Academy Programming Session • LIVE',
-    title: 'HiMark Academy is live on YouTube!',
+    category: "HiMark Academy Programming Session • LIVE",
+    title: "HiMark Academy is live on YouTube!",
     image:
-      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+      "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
   },
   {
-    category: 'Nigeria • Trending',
-    title:
-      'Naira Redesign by the Federal Government',
-    image: 'https://avatars.githubusercontent.com/u/17177659?s=280&v=4',
+    category: "Nigeria • Trending",
+    title: "Naira Redesign by the Federal Government",
+    image: "https://avatars.githubusercontent.com/u/17177659?s=280&v=4",
   },
   {
-    category: 'Web3.0 • Trending',
-    title: 'Blockchain developers make over $150k',
+    category: "Web3.0 • Trending",
+    title: "Blockchain developers make over $150k",
     image:
-      'https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/L424HUS65FGRVFPTYYPG24CIYI.jpg',
+      "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/L424HUS65FGRVFPTYYPG24CIYI.jpg",
   },
   {
-    category: 'Football • Trending',
-    title: 'World Cup in Qatar',
+    category: "Football • Trending",
+    title: "World Cup in Qatar",
     image:
-      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/site/18907/products/cIkAZXNyRZimRw4uqXVX_gDvYTtwQsC33KZqCtbpB_image.jpg',
+      "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/site/18907/products/cIkAZXNyRZimRw4uqXVX_gDvYTtwQsC33KZqCtbpB_image.jpg",
   },
-]
+];
 
 export const whoToFollow = [
   {
-    name: "Rafeh Qazi",
-    handle: "@cleverqazi",
+    name: "Elon Musk",
+    handle: "@elonmusk",
     avatar:
-      "https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
   },
   {
-    name: "David Rakosi",
-    handle: "@DavRakosi",
+    name: "Mark",
+    handle: "@markberg",
     avatar:
-      "https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg",
+      "https://www.biography.com/.image/t_share/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg", //1200x1200
   },
   {
-    name: "Sanity.io",
-    handle: "@sanity_io",
-    avatar: "https://avatars.githubusercontent.com/u/17177659?s=280&v=4",
+    name: "Wizkid",
+    handle: "@wizkidayo",
+    avatar: "https://iscale.iheart.com/catalog/artist/657868",
   },
 ];
