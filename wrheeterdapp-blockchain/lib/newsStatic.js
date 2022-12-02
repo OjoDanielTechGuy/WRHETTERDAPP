34893363358a4651e74d9150f8004efe02e63fd9
OJO DANIEL OLUWAGEBMIGA <danielbankole33@yahoo.com>
2022-12-02 14:54:23 +0000

    right section rendered in profile

---
+++ wrheeterdapp-blockchain/lib/newsStatic.js
@@ -0,0 +1,46 @@
+export const news = [
+  {
+    category: 'HiMark Academy Programming Session • LIVE',
+    title: 'HiMark Academy is live on YouTube!',
+    image:
+      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+  },
+  {
+    category: 'Nigeria • Trending',
+    title:
+      'Naira Redesign by the Federal Government',
+    image: 'https://avatars.githubusercontent.com/u/17177659?s=280&v=4',
+  },
+  {
+    category: 'Web3.0 • Trending',
+    title: 'Blockchain developers make over $150k',
+    image:
+      'https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/L424HUS65FGRVFPTYYPG24CIYI.jpg',
+  },
+  {
+    category: 'Football • Trending',
+    title: 'World Cup in Qatar',
+    image:
+      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/site/18907/products/cIkAZXNyRZimRw4uqXVX_gDvYTtwQsC33KZqCtbpB_image.jpg',
+  },
+]
+
+export const whoToFollow = [
+  {
+    name: "Rafeh Qazi",
+    handle: "@cleverqazi",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg",
+  },
+  {
+    name: "David Rakosi",
+    handle: "@DavRakosi",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg",
+  },
+  {
+    name: "Sanity.io",
+    handle: "@sanity_io",
+    avatar: "https://avatars.githubusercontent.com/u/17177659?s=280&v=4",
+  },
+];
