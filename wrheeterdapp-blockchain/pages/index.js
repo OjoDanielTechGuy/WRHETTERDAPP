81968d2109770480c7dd77bbe8c01f8f461ed950
OJO DANIEL OLUWAGEBMIGA <danielbankole33@yahoo.com>
2022-12-02 14:55:26 +0000

    js codes for displays

--- wrheeterdapp-blockchain/pages/index.js
+++ wrheeterdapp-blockchain/pages/index.js
@@ -1,18 +1,20 @@
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/home/Feed'
+import Widgets from '../components/Widgets'
 
 const style = {
-  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  // wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  wrapper: `flex justify-center select-none bg-[#15202b] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`,
-}
+};
  
 export default function Home() {
   return (
     <div className = {style.wrapper}>
       <div className = {style.content}>
-        <Sidebar/>
-        <h2>Sidebar</h2>
-        <h2>Feed</h2>
-        <h2>Widgets</h2>
+        <Sidebar/>       
+        <Feed/>
+        <Widgets/>
       </div>
     </div>
     )
